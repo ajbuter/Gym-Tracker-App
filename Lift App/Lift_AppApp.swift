854e0ad787026b40a42d50fb85c7fde5270a93c4
Aiden Buter <aiden.buter@gmail.com>
2025-11-06 15:27:17 -0600

    Initial Commit

---
+++ Lift App/Lift_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lift_AppApp.swift
+//  Lift App
+//
+//  Created by Aiden Buter on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Lift_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
